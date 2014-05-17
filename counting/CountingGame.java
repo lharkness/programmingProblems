35abd60b47b8f74f9af8db1bf15073e014a76817
Lee <lee@leeharkness.com>
2014-05-17 09:58:44 -0500

    initial working solution

--- counting/CountingGame.java
+++ counting/CountingGame.java
@@ -1,34 +1,66 @@
-import java.util.Arrays;
+iimport java.util.Arrays;
 
 public class CountingGame {
-	public static void main(String[] args) {
-		int numEntries = Integer.parseInt(args[0]);
-		int step = Integer.parseInt(args[1]);
-		int[] entries = new int[numEntries];
-		int[] removed = new int[numEntries];
-		int numRemoved = 0;
-		for (int i = 0; i < entries.length; i++) {
-			entries[i] = i + 1;
-		}
-		
-		int curIndex = 0;
-		while (numRemoved < numEntries) {
-			while (curIndex % step != 0) {
-				if (!Arrays.asList(removed).contains(entries[curIndex])) {
-					curIndex++;
-					if (curIndex >= numEntries) {
-						curIndex = 1;
-					}
-				}
-			}
-			
-			removed[numRemoved] = entries[curIndex];
-			numRemoved++;
-			curIndex++;
-		}
-		
-		for (int removedEntry : removed) {
-			System.out.println(removedEntry + " ");
-		}
-	}
+    public static void main(String[] args) {
+        int numEntries = Integer.parseInt(args[0]);
+        int step = Integer.parseInt(args[1]);
+
+        int[] entries = new int[numEntries + 2];
+        int[] removed = new int[numEntries + 2];
+
+        int numRemoved = 0;
+
+        for (int i = 0; i <= numEntries; i++) {
+            entries[i] = i;
+        }
+
+        int curEntryIndex = 1;
+        int curStep = 1;
+        while(numRemoved < numEntries) {
+            if (removed[curEntryIndex] != 0) {
+                curEntryIndex++;
+                if (curEntryIndex > numEntries) {
+                    int nextIndex = 1;
+                    while(removed[nextIndex] != 0) {
+                        nextIndex++;
+                    }
+                    curEntryIndex = nextIndex;
+                }
+                continue;
+            }
+            if (curStep % step == 0) {
+                numRemoved++;
+                removed[curEntryIndex] = numRemoved;
+                curStep = 1;
+                curEntryIndex++;
+                if (curEntryIndex > numEntries) {
+                    int nextIndex = 1;
+                    while(removed[nextIndex] != 0) {
+                        nextIndex++;
+                    }
+                    curEntryIndex = nextIndex;
+                }
+                continue;
+            }
+            curStep++;
+            curEntryIndex++;
+
+            if (curEntryIndex > numEntries) {
+                int nextIndex = 1;
+                while(removed[nextIndex] != 0) {
+                    nextIndex++;
+                }
+                curEntryIndex = nextIndex;
+            }
+            if (curStep > step) {
+                curStep = 1;
+            }
+        }
+
+        for (int i : removed) {
+            System.out.print(i + " ");
+        }
+
+
+    }
 }
